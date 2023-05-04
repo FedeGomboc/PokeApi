c41dfff361fec8fe9ba0b249fe036c47738eb35a
FEDERICO URIEL GOMBOC <47128235@ort>
2023-05-04 11:29:15 -0300

    tp avanzado

--- funciones.js
+++ funciones.js
@@ -7,22 +7,52 @@ function CargarPokemones() {
       pokemones.map((pokemon, index) => {
         const { name, url } = pokemon;
 
-        document.querySelector("#listado").innerHTML += `<div class="card" style="width: 18rem;">
+        document.querySelector("#listado").innerHTML += `<div class="card centerr columna" style="width: 18rem;">
 
         <img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index+1}.png " alt="Card image cap">
-        <div class="row">
-        <div class="col-4">
+     
         <div class="card-body">
           <h5 class="card-title">${name}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="#" onclick="CargarDatos('${url}',${index})" class="btn btn-success"  data-toggle="modal" data-target="#exampleModal">Go somewhere</a>
           </div>
-          </div>
-          </div>
+          
       </div>`;
       });
     })
     .catch((error) => {
       console.log(error);
     });
+}
+
+function CargarDatos(url, index){
+
+  axios
+  .get(url)
+  .then((result) => {
+    const datosPoke = result.data;
+    const { name, stats, types } = datosPoke
+    console.log(url)
+    document.querySelector("#datos").innerHTML = "";
+    document.querySelector("#datos").innerHTML += `<center><div class="card" style="width: 18rem;">
+    
+    <img class="card-img-top" id="img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png" alt="Card image cap">
+    <div class="card-body">
+      <h4 class="card-title pb-1">${name}</h4>
+      
+      <p class="card-text">HP: ${stats[0].base_stat}</p>
+     
+      <p class="card-text">Ataque: ${stats[1].base_stat}</p>
+    
+      <p class="card-text">Velocidad: ${stats[5].base_stat}</p>
+     
+      <p class="card-text">Tipo: ${datosPoke.types[0].type.name}</p>
+       </div>
+  </div><center>`
+
+  
+
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 }

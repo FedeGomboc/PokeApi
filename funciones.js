b5815584c661857eb006fa9a12781c37fb7c0f11
FedeGomboc <47128235@est.ort.edu.ar>
2023-05-04 19:50:03 -0300

    terminando

--- funciones.js
+++ funciones.js
@@ -1,21 +1,26 @@
 function CargarPokemones() {
+
+  document.querySelector("#boton").style.display = 'none';
   axios
-    .get("https://pokeapi.co/api/v2/pokemon?limit=20")
+    .get("https://pokeapi.co/api/v2/pokemon?limit=30")
     .then((result) => {
       const pokemones = result.data.results;
 
       pokemones.map((pokemon, index) => {
         const { name, url } = pokemon;
 
-        document.querySelector("#listado").innerHTML += `<div class="card centerr columna" style="width: 18rem;">
+        document.querySelector(
+          "#listado"
+        ).innerHTML += `<div class="card centerr columna rounded " style="width: 18rem;">
 
-        <img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index+1}.png " alt="Card image cap">
+        <img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+          index + 1
+        }.png " alt="Card image cap">
      
-        <div class="card-body">
+        <div class="card-body" >
           <h5 class="card-title">${name}</h5>
-          <a href="#" onclick="CargarDatos('${url}',${index})" class="btn btn-success"  data-toggle="modal" data-target="#exampleModal">Go somewhere</a>
+          <a href="#" onclick="CargarDatos('${url}',${index})" class="btn btn-success rounded"  data-toggle="modal" data-target="#exampleModal">Ver datos</a>
           </div>
-          
       </div>`;
       });
     })
@@ -24,35 +29,31 @@ function CargarPokemones() {
     });
 }
 
-function CargarDatos(url, index){
-
+function CargarDatos(url, index) {
   axios
-  .get(url)
-  .then((result) => {
-    const datosPoke = result.data;
-    const { name, stats, types } = datosPoke
-    console.log(url)
-    document.querySelector("#datos").innerHTML = "";
-    document.querySelector("#datos").innerHTML += `<center><div class="card" style="width: 18rem;">
+    .get(url)
+    .then((result) => {
+      const datosPokemon = result.data;
+      const { name, height, id, base_experience, weight} = datosPokemon;
+
+      document.querySelector("#info").innerHTML = "";
+      document.querySelector(
+        "#info"
+      ).innerHTML += `<center><div class="card">
     
-    <img class="card-img-top" id="img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png" alt="Card image cap">
+    <img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+      index + 1
+    }.png">
     <div class="card-body">
-      <h4 class="card-title pb-1">${name}</h4>
-      
-      <p class="card-text">HP: ${stats[0].base_stat}</p>
-     
-      <p class="card-text">Ataque: ${stats[1].base_stat}</p>
-    
-      <p class="card-text">Velocidad: ${stats[5].base_stat}</p>
-     
-      <p class="card-text">Tipo: ${datosPoke.types[0].type.name}</p>
+      <p class="card-text">Nombre: ${name}</p>
+      <p class="card-text">Numero de la pokedex: ${id}</p>
+      <p class="card-text">Altura: ${height/10} metros</p>
+      <p class="card-text">Peso: ${weight/10} kilogramos</p>
+      <p class="card-text">Experiencia: ${base_experience} XP</p>
        </div>
-  </div><center>`
-
-  
-
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+  </div><center>`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
